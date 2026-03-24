24f331ca878c41026898d8f7bfc8efc532bc6e81
Lubby H. <lubzymessi@gmail.com>
2026-03-24 14:47:44 +0200

    Initial

---
+++ script.js
@@ -0,0 +1,5 @@
+// Example: Show alert when form is submitted
+document.querySelector("form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Thank you for contacting Estcourt Recycling Centre!");
+});
